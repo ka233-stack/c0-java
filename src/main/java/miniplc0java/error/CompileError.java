7979e80bbcb1a8e1d551a1081f9d90ce31e0f1b6
ka233-stack <1183493652@qq.com>
2020-12-10 20:28:06 +0800

    ...

---
+++ src/main/java/miniplc0java/error/CompileError.java
@@ -0,0 +1,8 @@
+package miniplc0java.error;
+
+public abstract class CompileError extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public abstract ErrorCode getErr();
+}
