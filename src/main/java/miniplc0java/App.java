e899593894d4937c5e3051a9cbb742d5f70d212f
ka233-stack <1183493652@qq.com>
2020-12-10 22:29:47 +0800

    ..

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -2,6 +2,7 @@ package miniplc0java;
 
 import miniplc0java.analyser.Analyser;
 import miniplc0java.analyser.o0.O0;
+import miniplc0java.error.CompileError;
 import miniplc0java.tokenizer.StringIter;
 import miniplc0java.tokenizer.Tokenizer;
 
@@ -10,24 +11,19 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, CompileError {
         Scanner scanner = null;
         PrintStream output = null;
-        try {
-            scanner = new Scanner(new File(args[0]));
-            output = new PrintStream(new FileOutputStream(args[1]));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        scanner = new Scanner(new File(args[0]));
+        output = new PrintStream(new FileOutputStream(args[1]));
+
         StringIter stringIter = new StringIter(scanner);
         Tokenizer tokenizer = new Tokenizer(stringIter);
         Analyser analyser = new Analyser(tokenizer);
         O0 binCodeFile = null;
-        try {
-            binCodeFile = analyser.analyse();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        binCodeFile = analyser.analyse();
+
         String str;
         if (binCodeFile != null) {
             str = binCodeFile.toHexCode();
