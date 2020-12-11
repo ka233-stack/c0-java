79c04d9ee34c673937e9653d136623c4af52c3ee
ka233-stack <1183493652@qq.com>
2020-12-11 09:45:01 +0800

    v0.111

--- src/test/java/miniplc0java/AnalyserTest.java
+++ src/test/java/miniplc0java/AnalyserTest.java
@@ -31,8 +31,41 @@ public class AnalyserTest {
             e.printStackTrace();
         }
         System.out.println("\n");
-         System.out.println(binCodeFile.toString());
+//         System.out.println(binCodeFile.toString());
 //        System.out.println(binCodeFile.printString());
+        System.out.println("##########################################\n");
+        System.out.println(analyzer.getSymbolTable());
+    }
+
+    @Test
+    public void generateTest() {
+        Scanner scanner = null;
+        PrintStream output = null;
+        try {
+            scanner = new Scanner(new File("analyseTest.txt"));
+            output = new PrintStream(new FileOutputStream("anlyseTest.o0"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        StringIter stringIter = new StringIter(scanner);
+        Tokenizer tokenizer = new Tokenizer(stringIter);
+
+        // analyze
+        var analyzer = new Analyser(tokenizer);
+        O0 binCodeFile = null;
+        try {
+            binCodeFile = analyzer.analyse();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n");
+//         System.out.println(binCodeFile.toString());
+//        System.out.println(binCodeFile.printString());
+        try {
+            binCodeFile.writeFile(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("##########################################\n");
         System.out.println(analyzer.getSymbolTable());
     }
